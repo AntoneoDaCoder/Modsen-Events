c36b72cab3c18400e4c1c2c9cafe6b1322a9f0fe
Anton Urbanovich <antonurbanovic@gmail.com>
2025-01-24 15:06:43 +0300

    Determined project structure, added domain models

---
+++ Events.Core/Models/Event.cs
@@ -0,0 +1,34 @@
+﻿namespace Events.Core.Models
+{
+    public class Event
+    {
+        public Guid Id { get; }
+        public string Name { get; } = string.Empty;
+        public string Description { get; } = string.Empty;
+        public DateOnly Date { get; } = DateOnly.MinValue;
+        public TimeOnly Time { get; } = TimeOnly.MinValue;
+        public string Location { get; } = string.Empty;
+        public string Category { get; } = string.Empty;
+        public uint MaxParticipants { get; } = 0;
+        public ICollection<Participant> Participants { get; } = new List<Participant>();
+        private Event
+            (Guid id, string name, string description, DateOnly date, TimeOnly time,
+            string location, string category, uint maxParticipants, ICollection<Participant> participants)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Date = date;
+            Time = time;
+            Location = location;
+            Category = category;
+            MaxParticipants = maxParticipants;
+            Participants = participants;
+        }
+        static public Event CreateEvent(Guid id, string name, string description, DateOnly date, TimeOnly time,
+            string location, string category, uint maxParticipants, ICollection<Participant> participants)
+        {
+            return new Event(id, name, description, date, time, location, category, maxParticipants, participants);
+        }
+    }
+}
---
+++ Events.Core/Models/Participant.cs
@@ -0,0 +1,24 @@
+﻿namespace Events.Core.Models
+{
+    public class Participant
+    {
+        public Guid Id { get; }
+        public string Name { get; } = string.Empty;
+        public string Surname { get; } = string.Empty;
+        public DateOnly BirthDate { get; } = DateOnly.MinValue;
+        public DateOnly EventRegistrationDate { get; } = DateOnly.MinValue;
+        public string Email { get; } = string.Empty;
+        private Participant(Guid id, string name, string surname, DateOnly birthDate, DateOnly eventRegistrationDate, string email)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            BirthDate = birthDate;
+            EventRegistrationDate = eventRegistrationDate;
+        }
+        static public Participant CreateParticipant(Guid id, string name, string surname, DateOnly birthDate, DateOnly eventRegistrationDate, string email)
+        {
+            return new Participant(id, name, surname, birthDate, eventRegistrationDate, email);
+        }
+    }
+}
