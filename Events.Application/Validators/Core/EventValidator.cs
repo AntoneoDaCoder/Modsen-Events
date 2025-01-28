6ced1c64e87b2f500240f0afbd07bf390508ea5f
Anton Urbanovich <antonurbanovic@gmail.com>
2025-01-29 02:19:04 +0300

    Added new validator for domain model

---
+++ Events.Application/Validators/Core/EventValidator.cs
@@ -0,0 +1,22 @@
+﻿
+using FluentValidation;
+using Events.Core.Models;
+namespace Events.Application.Validators.Core
+{
+    public class EventValidator:AbstractValidator<Event>
+    {
+        public EventValidator()
+        {
+            RuleFor(e => e)
+                .Must(e => IsDateTimeInFuture(e.Date, e.Time))
+                .WithMessage("Event date and time cannot be in the past.");
+            RuleFor(e => e.MaxParticipants).InclusiveBetween((uint)1, (uint)10).WithMessage("The maximum number of participants must be in the range from 1 to 10 (inclusive).");
+        }
+
+        private bool IsDateTimeInFuture(DateOnly date, TimeOnly time)
+        {
+            var eventDateTime = date.ToDateTime(time); 
+            return eventDateTime >= DateTime.Now; 
+        }
+    }
+}
