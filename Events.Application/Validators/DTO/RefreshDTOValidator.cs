d599ae22ad740388d04807415db91c92c38da6c1
Anton Urbanovich <antonurbanovic@gmail.com>
2025-01-28 10:14:40 +0300

    Added refresh request dto

---
+++ Events.Application/Validators/DTO/RefreshDTOValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Events.Application.Contracts;
+namespace Events.Application.Validators.DTO
+{
+    public class RefreshDTOValidator : AbstractValidator<RefreshTokenDTO>
+    {
+        public RefreshDTOValidator()
+        {
+            RuleFor(x => x.AccessToken).NotEmpty();
+            RuleFor(x => x.RefreshToken).NotEmpty();
+        }
+    }
+}
