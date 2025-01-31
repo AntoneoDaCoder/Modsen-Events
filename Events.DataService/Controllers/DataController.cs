7aba8d813a58a34e87ba96fea251df40e0758d96
Anton Urbanovich <antonurbanovic@gmail.com>
2025-01-31 11:42:53 +0300

    finished data service

--- Events.DataService/Controllers/DataController.cs
+++ Events.DataService/Controllers/DataController.cs
@@ -6,6 +6,8 @@ using Events.Core.Models;
 using AutoMapper;
 using Events.Application.Extensions.Misc;
 using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Net.Http.Headers;
 namespace Events.DataService.Controllers
 {
     [ApiController]
@@ -16,124 +18,64 @@ namespace Events.DataService.Controllers
         private readonly IValidator<AddEventParticipantDTO> _addParticipantValidator;
         private readonly IValidator<CreateEventDTO> _createEventValidator;
         private readonly IValidator<IFormFile> _imgValidator;
-        private readonly IWebHostEnvironment _webEnvironment;
         private readonly IMapper _mapper;
         public DataController
             (IDataService dataService, IValidator<AddEventParticipantDTO> addParticipant,
-             IValidator<CreateEventDTO> createEvent, IValidator<IFormFile> imgValidator, IWebHostEnvironment web, IMapper mapper)
+             IValidator<CreateEventDTO> createEvent, IValidator<IFormFile> imgValidator, IMapper mapper)
         {
             _dataService = dataService;
             _addParticipantValidator = addParticipant;
             _createEventValidator = createEvent;
             _mapper = mapper;
             _imgValidator = imgValidator;
-            _webEnvironment = web;
-        }
-
-        [HttpPost]
-        [Route("api/events/{eventId}/participants")]
-        public async Task<IActionResult> RegisterEventParticipantAsync(string eventId, [FromBody] AddEventParticipantDTO newParticipant)
-        {
-            var validationResult = _addParticipantValidator.Validate(newParticipant);
-            if (validationResult.IsValid)
-            {
-                await _dataService.RegisterParticipantAsync(eventId, newParticipant.ParticipantId);
-                return StatusCode(201);
-            }
-            foreach (var error in validationResult.Errors)
-                ModelState.TryAddModelError(error.PropertyName, error.ErrorMessage);
-            return BadRequest(ModelState);
         }
         [HttpGet]
-        [Route("api/events/{eventId}/participants")]
-        public async Task<IActionResult> GetPagedEventParticipantsAsync([FromQuery] int pageSize, [FromQuery] int page, string eventId)
+        [Route("{eventId:guid}/participants")]
+        public async Task<IActionResult> GetPagedEventParticipantsAsync([FromQuery(Name = "pageSize")] int pageSize, [FromQuery(Name = "page")] int page, Guid eventId)
         {
-            var participants = await _dataService.GetPagedParticipantsAsync(eventId, page, pageSize);
+            var participants = await _dataService.GetPagedParticipantsAsync(eventId.ToString(), page, pageSize);
             if (participants.Count > 0)
                 return StatusCode(200, participants);
-            return StatusCode(200, "Couldn't find any participants.");
+            return NotFound("Couldn't find any participants.");
         }
         [HttpGet]
-        [Route("api/events/{eventId}/participants/{participantId}")]
-        public async Task<IActionResult> GetEventParticipantByIdAsync(string eventId, string participantId)
+        [Route("{eventId:guid}/participants/{participantId:guid}")]
+        public async Task<IActionResult> GetEventParticipantByIdAsync(Guid eventId, Guid participantId)
         {
-            var participant = await _dataService.GetEventParticipantByIdAsync(eventId, participantId);
+            var participant = await _dataService.GetEventParticipantByIdAsync(eventId.ToString(), participantId.ToString());
             if (participant is null)
-                return StatusCode(200, "Event doesn't exist or participant isn't registered for this event.");
+                return NotFound("Event doesn't exist or participant isn't registered for this event.");
             return StatusCode(200, participant);
         }
-        [HttpDelete]
-        [Route("api/events/{eventId}/participants/{participantId}")]
-        public async Task<IActionResult> UnregisterParticipantAsync(string eventId, string participantId)
-        {
-            await _dataService.UnregisterParticipantAsync(eventId, participantId);
-            return StatusCode(200);
-        }
         [HttpGet]
-        [Route("api/events")]
-        public async Task<IActionResult> GetPagedEventsAsync([FromQuery] int page, [FromQuery] int pageSize)
+        [Route("all")]
+        public async Task<IActionResult> GetPagedEventsAsync([FromQuery(Name = "page")] int page, [FromQuery(Name = "pageSize")] int pageSize)
         {
             var events = await _dataService.GetPagedEventsAsync(page, pageSize);
             if (events.Count > 0)
                 return StatusCode(200, events);
-            return StatusCode(200, "There are no events.");
+            return NotFound("There are no events.");
         }
         [HttpGet]
-        [Route("api/events/{eventId}")]
-        public async Task<IActionResult> GetEventByIdAsync(string eventId)
+        [Route("{eventId:guid}")]
+        public async Task<IActionResult> GetEventByIdAsync(Guid eventId)
         {
-            var @event = await _dataService.GetEventByIdAsync(eventId);
+            var @event = await _dataService.GetEventByIdAsync(eventId.ToString());
             if (@event is null)
-                return StatusCode(200, "Such event doesn't exist.");
+                return NotFound("Such event doesn't exist.");
             return StatusCode(200, @event);
         }
         [HttpGet]
-        [Route("api/events/")]
-        public async Task<IActionResult> GetEventByNameAsync([FromQuery] string name)
+        [Route("")]
+        public async Task<IActionResult> GetEventByNameAsync([FromQuery(Name = "name")] string name)
         {
             var @event = await _dataService.GetEventByNameAsync(name);
             if (@event is null)
-                return StatusCode(200, "Such event doesn't exist.");
+                return NotFound("Such event doesn't exist.");
             return StatusCode(200, @event);
         }
-        [HttpPost]
-        [Route("api/events")]
-        public async Task<IActionResult> CreateEventAsync([FromBody] CreateEventDTO newEvent)
-        {
-            var validationResult = _createEventValidator.Validate(newEvent);
-            if (validationResult.IsValid)
-            {
-
-                await _dataService.CreateEventAsync(_mapper.Map<Event>(newEvent));
-                return StatusCode(201);
-            }
-            foreach (var error in validationResult.Errors)
-                ModelState.TryAddModelError(error.PropertyName, error.ErrorMessage);
-            return BadRequest(ModelState);
-        }
-        [HttpPut]
-        [Route("api/events/{eventId}")]
-        public async Task<IActionResult> UpdateEventAsync(string eventId, [FromBody] CreateEventDTO newEvent)
-        {
-            var validationResult = _createEventValidator.Validate(newEvent);
-            if (validationResult.IsValid)
-            {
-                await _dataService.UpdateEventAsync(eventId, _mapper.Map<Event>(newEvent));
-                return StatusCode(200);
-            }
-            foreach (var error in validationResult.Errors)
-                ModelState.TryAddModelError(error.PropertyName, error.ErrorMessage);
-            return BadRequest(ModelState);
-        }
-        [HttpDelete]
-        [Route("api/events/{eventId}")]
-        public async Task<IActionResult> DeleteEventAsync(string eventId)
-        {
-            await _dataService.DeleteEventAsync(eventId);
-            return StatusCode(200);
-        }
         [HttpGet]
-        [Route("api/events/filter")]
+        [Route("filter")]
         public async Task<IActionResult> GetPagedEventsByCriterionAsync
             ([FromQuery] DateOnly? date, [FromQuery] string? location, [FromQuery] string? category, [FromQuery] int page, [FromQuery] int pageSize)
         {
@@ -151,25 +93,112 @@ namespace Events.DataService.Controllers
             var res = await _dataService.GetPagedEventsByCriterionAsync(PredicateExtensions.CombinePredicates(predicates), page, pageSize);
             if (res.Count > 0)
                 return StatusCode(200, res);
-            return StatusCode(200, "No events meet specified criteria.");
+            return NotFound("No events meet specified criteria.");
+        }
+
+        [HttpGet]
+        [Route("{eventId:guid}/image")]
+        public async Task<IActionResult> GetEventImageAsync(Guid eventId)
+        {
+            var rootDir = Environment.GetEnvironmentVariable("ROOT_DIR");
+            var webRootPath = Environment.GetEnvironmentVariable("UPLOAD_DIR");
+            byte[] imgBytes = await _dataService.GetEventImageAsync(eventId.ToString(), webRootPath!, rootDir!);
+            if (imgBytes.Length > 0)
+            {
+                string eTag = $"\"{Convert.ToBase64String(MD5.HashData(imgBytes))}\"";
+                Console.WriteLine(eTag);
+                var clientETag = Request.Headers.IfNoneMatch.FirstOrDefault();
+                Console.WriteLine(clientETag);
+                if (clientETag != null && clientETag == eTag)
+                    return StatusCode(304);
+                Response.Headers.ETag = eTag;
+                Response.Headers.CacheControl = new CacheControlHeaderValue
+                {
+                    Public = true,
+                    MaxAge = TimeSpan.FromMinutes(10)
+                }.ToString();
+                return File(imgBytes, "image/png");
+            }
+            return NotFound("Image for this event doesn't exist.");
         }
         [HttpPost]
-        [Route("api/events/{eventId}")]
-        public async Task<IActionResult> SaveEventImageAsync(string eventId, [FromBody] IFormFile img)
+        [Route("{eventId:guid}/participants")]
+        public async Task<IActionResult> RegisterEventParticipantAsync(Guid eventId, [FromBody] AddEventParticipantDTO newParticipant)
         {
-            var validationResult = _imgValidator.Validate(img);
+            var validationResult = _addParticipantValidator.Validate(newParticipant);
             if (validationResult.IsValid)
             {
-                var rootDir = Environment.GetEnvironmentVariable("ROOT_DIR");
-                using (var stream = img.OpenReadStream())
-                {
-                    await _dataService.SaveEventImageAsync(eventId, _webEnvironment.WebRootPath, rootDir!, stream);
-                }
-                return StatusCode(201);
+                await _dataService.RegisterParticipantAsync(eventId.ToString(), newParticipant.ParticipantId);
+                return StatusCode(201, "Participant has been registered for event");
             }
             foreach (var error in validationResult.Errors)
                 ModelState.TryAddModelError(error.PropertyName, error.ErrorMessage);
             return BadRequest(ModelState);
         }
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> CreateEventAsync([FromBody] CreateEventDTO newEvent)
+        {
+            var validationResult = _createEventValidator.Validate(newEvent);
+            if (validationResult.IsValid)
+            {
+                await _dataService.CreateEventAsync(_mapper.Map<Event>(newEvent));
+                return StatusCode(201, "New event has been added.");
+            }
+            foreach (var error in validationResult.Errors)
+                ModelState.TryAddModelError(error.PropertyName, error.ErrorMessage);
+            return BadRequest(ModelState);
+        }
+        [HttpPost]
+        [Route("{eventId:guid}")]
+        public async Task<IActionResult> SaveEventImageAsync(Guid eventId, [FromForm(Name = "Image")] IFormFile image)
+        {
+            var validationResult = _imgValidator.Validate(image);
+            if (validationResult.IsValid)
+            {
+                var rootDir = Environment.GetEnvironmentVariable("ROOT_DIR");
+                var webRootPath = Environment.GetEnvironmentVariable("UPLOAD_DIR");
+                using (var stream = image.OpenReadStream())
+                {
+                    await _dataService.SaveEventImageAsync(eventId.ToString(), webRootPath!, rootDir!, stream);
+                }
+                return StatusCode(201, "Image has been uploaded.");
+            }
+            foreach (var error in validationResult.Errors)
+                ModelState.TryAddModelError(error.PropertyName, error.ErrorMessage);
+            return BadRequest(ModelState);
+        }
+        [HttpPut]
+        [Route("{eventId:guid}")]
+        public async Task<IActionResult> UpdateEventAsync(Guid eventId, [FromBody] CreateEventDTO newEvent)
+        {
+            var validationResult = _createEventValidator.Validate(newEvent);
+            if (validationResult.IsValid)
+            {
+                await _dataService.UpdateEventAsync(eventId.ToString(), _mapper.Map<Event>(newEvent));
+                return StatusCode(200, "Event successfully updated.");
+            }
+            foreach (var error in validationResult.Errors)
+                ModelState.TryAddModelError(error.PropertyName, error.ErrorMessage);
+            return BadRequest(ModelState);
+        }
+        [HttpDelete]
+        [Route("{eventId:guid}/participants/{participantId:guid}")]
+        public async Task<IActionResult> UnregisterParticipantAsync(Guid eventId, Guid participantId)
+        {
+            await _dataService.UnregisterParticipantAsync(eventId.ToString(), participantId.ToString());
+            return StatusCode(200, "Successfully unregistered participant from event.");
+        }
+        [HttpDelete]
+        [Route("{eventId:guid}")]
+        public async Task<IActionResult> DeleteEventAsync(Guid eventId)
+        {
+            var rootDir = Environment.GetEnvironmentVariable("ROOT_DIR");
+            var webRootPath = Environment.GetEnvironmentVariable("UPLOAD_DIR");
+            await _dataService.DeleteEventImageAsync(eventId.ToString(), webRootPath!, rootDir!);
+            await _dataService.DeleteEventAsync(eventId.ToString());
+            return StatusCode(200, "Event successfully deleted.");
+        }
+
     }
 }
--- Events.DataService/Program.cs
+++ Events.DataService/Program.cs
@@ -1,21 +1,20 @@
+using Events.Application.Extensions.Data;
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.ConfigureServices();
+builder.Services.ConfigureDbContext();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.ConfigureMiddleware();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
