08145951968bef546eae8981249650b7bf2cf5bf
Anton Urbanovich <antonurbanovic@gmail.com>
2025-01-31 11:44:41 +0300

    Added page validator for data service controller

---
+++ Events.Application/Validators/Core/PageValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Events.Application.Validators.Core
+{
+    public class PageValidator:AbstractValidator<(int,int)>
+    {
+        public PageValidator()
+        {
+            RuleFor(v => v.Item1).GreaterThanOrEqualTo(1).WithMessage("Page number should be 1 or greater.");
+            RuleFor(v => v.Item2).GreaterThanOrEqualTo(1).WithMessage("Page size should be 1 or greater.");
+        }
+    }
+}
